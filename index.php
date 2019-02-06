528021a5becae8edb776fa2e4fa78e810956f644
Profute <peterthygesen1@gmail.com>
2019-02-06 15:06:20 +0100

    index and stylesheet has been edited

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php 
-echo "test";
+<?php
 ?>
+<!DOCTYPE html> 
+<html lang="en">
+
+	<head>
+		<title>
+			Music Site
+		</title>
+	
+	
+	
+		<link rel="stylesheet" type="text/css" href="Style.css">
+		<meta charset="UTF-8">
+	</head>
+	
+
+	<body>
+		
+		fgurtyguilsdtgulkoijhfgh
+	</body>
+	
+</html>

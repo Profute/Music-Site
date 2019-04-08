5ae9f3c206d011d60f27d09fc6916ab97dc59fd2
Marc Hansen <marc@sabater.dk>
2019-04-08 11:11:28 +0200

    Update database.php

--- database.php
+++ database.php
@@ -161,30 +161,52 @@
 	} else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 	} 
-	$sql = "INSERT INTO Nummer (Lyric, Title)
-	VALUES ('', '')";
+
+	$sql = "INSERT INTO Artist (ArtistNavn, nationalitet, foeselsdato)
+	VALUES ('', '', '')";
 
 	if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 	} else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 	} 
-	$sql = "INSERT INTO Nummer (Lyric, Title)
-	VALUES ('', '')";
+
+	$sql = "INSERT INTO Pladeselskab (Navn)
+	VALUES ('')";
 
 	if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 	} else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 	} 
-	$sql = "INSERT INTO Nummer (Lyric, Title)
+
+	$sql = "INSERT INTO Band (bandNavn,	nationalitet, dannelse)
+	VALUES ('', '', '')";
+
+	if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+	} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+
+	$sql = "INSERT INTO Album (Titel, Genre)
 	VALUES ('', '')";
 
 	if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 	} else {
     echo "Error: " . $sql . "<br>" . $conn->error;
-	} 
+	}
+
+	$sql = "INSERT INTO Single (Titel, Genre)
+	VALUES ('', '')";
+
+	if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+	} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+
 	
 ?>
  

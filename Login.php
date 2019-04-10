3bfa17e9e910f8af31d1bea3dbb1fec7916dce74
Profute <peterthygesen1@gmail.com>
2019-04-10 15:59:03 +0200

    Update Login.php

--- Login.php
+++ Login.php
@@ -3,19 +3,23 @@ session_start();
 $_SESSION["Welcome"] = true;
 $_SESSION["brugerID"] = "uBrugerID";
 $servername = "localhost";
-$dbname = "Music_Site";  
-$username = $_GET["bNavn"];                  
-$password = $_GET["pword"];  
-// Create connection
+$dbname = "Music_Site";
+//Starter en session og definerer databasensnavn og servernavent
 
-
+//Afhængigt af hvad username og password angiver den hvilken bruger er logget ind
+if($_SESSION["Welcome"] = true){
+	$username = 'Peasant';
+	$password = 1234;
+} else {
+	$username = $_POST["UName"];                  
+	$password = $_POST["PWord"];  
+}
 
 // Create connection
 $conn = new mysqli($servername, $username, $password,$dbname);
 if ($conn->connect_error) {
     die("Login failed: " . $conn->connect_error);
-    header("Location : http://localhost:8000/MUSIC-SITE/index.php");
+    header("Location : C:\Abyss Web Server\htdocs\Music-Site\pages");
     
 } 
 echo "Login successfully <br>";
-
